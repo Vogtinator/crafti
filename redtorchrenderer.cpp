34f558e7dcf5a7a77b5f46d560583bd7dc71274e
Fabian Vogt <fabian@ritter-vogt.de>
2014-06-06 20:30:03 +0200

    Unite both torch render methods

--- redtorchrenderer.cpp
+++ redtorchrenderer.cpp
@@ -4,56 +4,7 @@ void RedstoneTorchRenderer::renderSpecialBlock(const BLOCK_WDATA block, GLFix x,
 {
     TextureAtlasEntry &tex = getPOWERSTATE(block) ? terrain_atlas[3][6].current : terrain_atlas[3][7].current;
 
-    glPushMatrix();
-    glLoadIdentity();
-
-    glTranslatef(x + BLOCK_SIZE/2, y + BLOCK_SIZE/2, z + BLOCK_SIZE/2);
-
-    std::vector<VERTEX> torch_vertices;
-    torch_vertices.reserve(16);
-
-    torch_vertices.push_back({0, 0, BLOCK_SIZE/2, tex.left, tex.bottom, 0xFFFF});
-    torch_vertices.push_back({0, BLOCK_SIZE, BLOCK_SIZE/2, tex.left, tex.top, 0xFFFF});
-    torch_vertices.push_back({BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE/2, tex.right, tex.top, 0xFFFF});
-    torch_vertices.push_back({BLOCK_SIZE, 0, BLOCK_SIZE/2, tex.right, tex.bottom, 0xFFFF});
-
-    torch_vertices.push_back({BLOCK_SIZE/2, 0, BLOCK_SIZE, tex.left, tex.bottom, 0xFFFF});
-    torch_vertices.push_back({BLOCK_SIZE/2, BLOCK_SIZE, BLOCK_SIZE, tex.left, tex.top, 0xFFFF});
-    torch_vertices.push_back({BLOCK_SIZE/2, BLOCK_SIZE, 0, tex.right, tex.top, 0xFFFF});
-    torch_vertices.push_back({BLOCK_SIZE/2, 0, 0, tex.right, tex.bottom, 0xFFFF});
-
-    switch(static_cast<BLOCK_SIDE>(getBLOCKDATA(block)))
-    {
-    default:
-    case BLOCK_TOP:
-        break;
-    case BLOCK_BOTTOM:
-        nglRotateX(180);
-        break;
-    case BLOCK_BACK:
-        nglRotateX(45);
-        break;
-    case BLOCK_FRONT:
-        nglRotateX(315);
-        break;
-    case BLOCK_LEFT:
-        nglRotateZ(45);
-        break;
-    case BLOCK_RIGHT:
-        nglRotateZ(315);
-        break;
-    }
-
-    glTranslatef(-BLOCK_SIZE / 2, -BLOCK_SIZE / 2, -BLOCK_SIZE / 2);
-
-    for(auto&& v : torch_vertices)
-    {
-        VERTEX v1;
-        nglMultMatVectRes(transformation, &v, &v1);
-        c.addUnalignedVertex(v1.x, v1.y, v1.z, v.u, v.v, v.c);
-    }
-
-    glPopMatrix();
+    TorchRenderer::renderTorch(static_cast<BLOCK_SIDE>(getBLOCKDATA(block)), x, y, z, tex, c);
 }
 
 void RedstoneTorchRenderer::drawPreview(const BLOCK_WDATA /*block*/, TEXTURE &dest, int x, int y)
