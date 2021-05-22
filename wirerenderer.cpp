ecf1f97011ad00b3c42060147dbf01b6ee542bc0
Fabian Vogt <fabian@ritter-vogt.de>
2021-05-22 21:54:20 +0200

    More minecraft-like redstone
    Totally broken. Chunk::isBlockPowered needs to be rethought.

--- wirerenderer.cpp
+++ wirerenderer.cpp
@@ -1,8 +1,45 @@
 #include "wirerenderer.h"
 
+struct Pos { int x, y, z; };
+struct AdjacentRedstone {
+	int count = 0;
+	Pos positions[4];
+	void add(const Pos &pos) { positions[count++] = pos; };
+};
+
+void getAdjacentRedstone(int local_x, int local_y, int local_z, Chunk &c, AdjacentRedstone &ar)
+{
+	bool go_up = getBLOCK(c.getGlobalBlockRelative(local_x, local_y + 1, local_z)) == BLOCK_AIR;
+	for(auto pos : {Pos{-1, 0, 0}, {1, 0, 0}, {0, 0, -1}, {0, 0, 1}})
+	{
+		auto x = local_x + pos.x;
+		auto y = local_y + pos.y;
+		auto z = local_z + pos.z;
+		auto this_block = c.getGlobalBlockRelative(x, y, z);
+		printf("(%d,%d,%d, %d, %d, %d) ", x, y, z, this_block, c.getGlobalBlockRelative(x, y - 1, z), getBLOCK(c.getGlobalBlockRelative(x, y - 1, z)));
+		if(getBLOCK(this_block) == BLOCK_REDSTONE_WIRE)
+			ar.add(Pos{x, y + 0, z});
+		else if(go_up && global_block_renderer.isObstacle(this_block) && getBLOCK(c.getGlobalBlockRelative(x, y + 1, z)) == BLOCK_REDSTONE_WIRE)
+			ar.add(Pos{x, y + 1, z});
+		else if(getBLOCK(this_block) == BLOCK_AIR && getBLOCK(c.getGlobalBlockRelative(x, y - 1, z)) == BLOCK_REDSTONE_WIRE)
+			ar.add(Pos{x, y - 1, z});
+	}
+	printf("AR: ");
+	for(int i = 0; i < ar.count; ++i)
+		printf("(%d,%d,%d) ", ar.positions[i].x, ar.positions[i].y, ar.positions[i].z);
+	printf("\n");
+}
+
 void WireRenderer::renderSpecialBlock(const BLOCK_WDATA block, GLFix x, GLFix y, GLFix z, Chunk &c)
 {
-    renderBillboard(x / BLOCK_SIZE, y / BLOCK_SIZE, z / BLOCK_SIZE, getPOWERSTATE(block) ? terrain_atlas[4][11].current : terrain_atlas[4][10].current, c);
+    //renderBillboard(x / BLOCK_SIZE, y / BLOCK_SIZE, z / BLOCK_SIZE, getPOWERSTATE(block) ? terrain_atlas[4][11].current : terrain_atlas[4][10].current, c);
+	const TextureAtlasEntry &tex = terrain_atlas[4][getPOWERSTATE(block) ? 11 : 10].current;
+	const GLFix height = BLOCK_SIZE / 16;
+
+    c.addUnalignedVertex(x, y + height, z, tex.left, tex.bottom, TEXTURE_DRAW_BACKFACE | TEXTURE_TRANSPARENT);
+    c.addUnalignedVertex(x, y + height, z + BLOCK_SIZE, tex.left, tex.top, TEXTURE_DRAW_BACKFACE | TEXTURE_TRANSPARENT);
+    c.addUnalignedVertex(x + BLOCK_SIZE, y + height, z + BLOCK_SIZE, tex.right, tex.top, TEXTURE_DRAW_BACKFACE | TEXTURE_TRANSPARENT);
+    c.addUnalignedVertex(x + BLOCK_SIZE, y + height, z, tex.right, tex.bottom, TEXTURE_DRAW_BACKFACE | TEXTURE_TRANSPARENT);
 }
 
 void WireRenderer::drawPreview(const BLOCK_WDATA /*block*/, TEXTURE &dest, const int x, const int y)
@@ -17,23 +54,14 @@ void WireRenderer::removedBlock(const BLOCK_WDATA block, int local_x, int local_
         return;
 
     //But now there may be different circuits, so check them seperately
-    if(getBLOCK(c.getGlobalBlockRelative(local_x - 1, local_y, local_z)) == BLOCK_REDSTONE_WIRE && !isActiveLeft(local_x - 1, local_y, local_z, c))
-        setCircuitState(false, local_x - 1, local_y, local_z, c);
-
-    if(getBLOCK(c.getGlobalBlockRelative(local_x + 1, local_y, local_z)) == BLOCK_REDSTONE_WIRE && !isActiveLeft(local_x + 1, local_y, local_z, c))
-        setCircuitState(false, local_x + 1, local_y, local_z, c);
-
-    if(getBLOCK(c.getGlobalBlockRelative(local_x, local_y - 1, local_z)) == BLOCK_REDSTONE_WIRE && !isActiveLeft(local_x, local_y - 1, local_z, c))
-        setCircuitState(false, local_x, local_y - 1, local_z, c);
-
-    if(getBLOCK(c.getGlobalBlockRelative(local_x, local_y + 1, local_z)) == BLOCK_REDSTONE_WIRE && !isActiveLeft(local_x, local_y + 1, local_z, c))
-        setCircuitState(false, local_x, local_y + 1, local_z, c);
-
-    if(getBLOCK(c.getGlobalBlockRelative(local_x, local_y, local_z - 1)) == BLOCK_REDSTONE_WIRE && !isActiveLeft(local_x, local_y, local_z - 1, c))
-        setCircuitState(false, local_x, local_y, local_z - 1, c);
-
-    if(getBLOCK(c.getGlobalBlockRelative(local_x, local_y, local_z + 1)) == BLOCK_REDSTONE_WIRE && !isActiveLeft(local_x, local_y, local_z + 1, c))
-        setCircuitState(false, local_x, local_y, local_z + 1, c);
+	AdjacentRedstone ar;
+	getAdjacentRedstone(local_x, local_y, local_z, c, ar);
+	for(int i = 0; i < ar.count; ++i)
+	{
+		Pos &pos = ar.positions[i];
+		if(getBLOCK(c.getGlobalBlockRelative(pos.x, pos.y, pos.z)) == BLOCK_REDSTONE_WIRE && !isActiveLeft(pos.y, pos.y, pos.z, c))
+			setCircuitState(false, pos.y, pos.y, pos.z, c);
+	}
 }
 
 void WireRenderer::addedBlock(const BLOCK_WDATA block, int local_x, int local_y, int local_z, Chunk &c)
@@ -96,35 +124,25 @@ void WireRenderer::setCircuitState(const bool state, const int local_x, const in
     BLOCK_WDATA block = c.getGlobalBlockRelative(local_x, local_y, local_z);
     c.setGlobalBlockRelative(local_x, local_y, local_z, getBLOCKWDATAPower(block, getBLOCKDATA(block), state));
 
-    if(getPOWERSTATE(block = c.getGlobalBlockRelative(local_x - 1, local_y, local_z)) != state && getBLOCK(block) == BLOCK_REDSTONE_WIRE)
-        setCircuitState(state, local_x - 1, local_y, local_z, c);
-    if(getPOWERSTATE(block = c.getGlobalBlockRelative(local_x + 1, local_y, local_z)) != state && getBLOCK(block) == BLOCK_REDSTONE_WIRE)
-        setCircuitState(state, local_x + 1, local_y, local_z, c);
-    if(getPOWERSTATE(block = c.getGlobalBlockRelative(local_x, local_y - 1, local_z)) != state && getBLOCK(block) == BLOCK_REDSTONE_WIRE)
-        setCircuitState(state, local_x, local_y - 1, local_z, c);
-    if(getPOWERSTATE(block = c.getGlobalBlockRelative(local_x, local_y + 1, local_z)) != state && getBLOCK(block) == BLOCK_REDSTONE_WIRE)
-        setCircuitState(state, local_x, local_y + 1, local_z, c);
-    if(getPOWERSTATE(block = c.getGlobalBlockRelative(local_x, local_y, local_z - 1)) != state && getBLOCK(block) == BLOCK_REDSTONE_WIRE)
-        setCircuitState(state, local_x, local_y, local_z - 1, c);
-    if(getPOWERSTATE(block = c.getGlobalBlockRelative(local_x, local_y, local_z + 1)) != state && getBLOCK(block) == BLOCK_REDSTONE_WIRE)
-        setCircuitState(state, local_x, local_y, local_z + 1, c);
+	AdjacentRedstone ar;
+	getAdjacentRedstone(local_x, local_y, local_z, c, ar);
+	for(int i = 0; i < ar.count; ++i)
+	{
+		Pos &pos = ar.positions[i];
+		BLOCK_WDATA block = c.getGlobalBlockRelative(local_x + pos.x, local_y + pos.y, local_z + pos.z);
+		if(getPOWERSTATE(block) != state)
+			setCircuitState(state, local_x + pos.x, local_y + pos.y, local_z + pos.z, c);
+	}
 }
 
 bool WireRenderer::isDirectlyPowered(const int local_x, const int local_y, const int local_z, Chunk &c)
 {
-    BLOCK_WDATA block;
-    if(getPOWERSTATE(block = c.getGlobalBlockRelative(local_x - 1, local_y, local_z)) == true && getBLOCK(block) != BLOCK_REDSTONE_WIRE)
-        return true;
-    if(getPOWERSTATE(block = c.getGlobalBlockRelative(local_x + 1, local_y, local_z)) == true && getBLOCK(block) != BLOCK_REDSTONE_WIRE)
-        return true;
-    if(getPOWERSTATE(block = c.getGlobalBlockRelative(local_x, local_y - 1, local_z)) == true && getBLOCK(block) != BLOCK_REDSTONE_WIRE)
-        return true;
-    if(getPOWERSTATE(block = c.getGlobalBlockRelative(local_x, local_y + 1, local_z)) == true && getBLOCK(block) != BLOCK_REDSTONE_WIRE)
-        return true;
-    if(getPOWERSTATE(block = c.getGlobalBlockRelative(local_x, local_y, local_z - 1)) == true && getBLOCK(block) != BLOCK_REDSTONE_WIRE)
-        return true;
-    if(getPOWERSTATE(block = c.getGlobalBlockRelative(local_x, local_y, local_z + 1)) == true && getBLOCK(block) != BLOCK_REDSTONE_WIRE)
-        return true;
+	for(auto pos : {Pos{-1, 0, 0}, {1, 0, 0}, {0, 0, -1}, {0, 0, 1}, {0, -1, 0}})
+	{
+		BLOCK_WDATA block = c.getGlobalBlockRelative(local_x + pos.x, local_y + pos.y, local_z + pos.z);
+		if(getPOWERSTATE(block) == true && getBLOCK(block) != BLOCK_REDSTONE_WIRE)
+			return true;
+	}
 
     return false;
 }
@@ -144,49 +162,18 @@ bool WireRenderer::isActiveLeft(const int local_x, const int local_y, const int 
 
     bool ret = false;
 
-    if(getBLOCK(c.getGlobalBlockRelative(local_x - 1, local_y, local_z)) == BLOCK_REDSTONE_WIRE)
-        if(isActiveLeft(local_x - 1, local_y, local_z, c))
-        {
+	AdjacentRedstone ar;
+	getAdjacentRedstone(local_x, local_y, local_z, c, ar);
+	for(int i = 0; i < ar.count; ++i)
+	{
+		Pos &pos = ar.positions[i];
+		if(getBLOCK(c.getGlobalBlockRelative(local_x + pos.x, local_y + pos.y, local_z + pos.z)) == BLOCK_REDSTONE_WIRE
+		   && isActiveLeft(local_x + pos.x, local_y + pos.y, local_z + pos.z, c))
+		{
             ret = true;
-            goto exit;
-        }
-
-    if(getBLOCK(c.getGlobalBlockRelative(local_x + 1, local_y, local_z)) == BLOCK_REDSTONE_WIRE)
-        if(isActiveLeft(local_x + 1, local_y, local_z, c))
-        {
-            ret = true;
-            goto exit;
-        }
-
-    if(getBLOCK(c.getGlobalBlockRelative(local_x, local_y - 1, local_z)) == BLOCK_REDSTONE_WIRE)
-        if(isActiveLeft(local_x, local_y - 1, local_z, c))
-        {
-            ret = true;
-            goto exit;
-        }
-
-    if(getBLOCK(c.getGlobalBlockRelative(local_x, local_y + 1, local_z)) == BLOCK_REDSTONE_WIRE)
-        if(isActiveLeft(local_x, local_y + 1, local_z, c))
-        {
-            ret = true;
-            goto exit;
-        }
-
-    if(getBLOCK(c.getGlobalBlockRelative(local_x, local_y, local_z - 1)) == BLOCK_REDSTONE_WIRE)
-        if(isActiveLeft(local_x, local_y, local_z - 1, c))
-        {
-            ret = true;
-            goto exit;
-        }
-
-    if(getBLOCK(c.getGlobalBlockRelative(local_x, local_y, local_z + 1)) == BLOCK_REDSTONE_WIRE)
-        if(isActiveLeft(local_x, local_y, local_z + 1, c))
-        {
-            ret = true;
-            goto exit;
-        }
-
-    exit:
+            break;
+		}
+	}
     c.setGlobalBlockRelative(local_x, local_y, local_z, block & ~(VISITED_BIT << 8), false);
 
     return ret;
