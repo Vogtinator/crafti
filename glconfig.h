8727aedc2a657ac943a70fac8d4c6e77c8d479f4
Fabian Vogt <fabian@ritter-vogt.de>
2021-06-20 21:39:18 +0200

    Update nGL
    SAFE_MODE is no longer optional.

--- glconfig.h
+++ glconfig.h
@@ -9,15 +9,10 @@
 //If disabled, triangles partially behind the CLIP_PLANE will be discarded
 #define Z_CLIPPING
 
-//Deactivate after you don't get any warnings.
-//Otherwise it may corrupt random memory and crash.
-//#define SAFE_MODE
-
 //If some geometry inaccuracies annoy you, enable this.
 //It's a bit slower though.
 //#define BETTER_PERSPECTIVE
 
-//Crashes sometimes, only works on CX
 #define FPS_COUNTER
 
 #if defined(TEXTURE_SUPPORT) && defined(INTERPOLATE_COLORS)
