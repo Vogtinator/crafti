bdb0d977ea776f0e35b039dea3cddaa200633e9c
Fabian Vogt <fabian@ritter-vogt.de>
2022-01-09 17:57:07 +0100

    Simplify TNT explosion

--- tntrenderer.cpp
+++ tntrenderer.cpp
@@ -13,20 +13,24 @@ void TNTRenderer::explode(const int local_x, const int local_y, const int local_
 {
     c.setGlobalBlockRelative(local_x, local_y, local_z, BLOCK_AIR);
 
-    for(int dist = 1; dist <= 3; ++dist)
-    {
-        for(int x = -dist; x <= dist; ++x)
-            for(int y = -dist; y <= dist; ++y)
-                for(int z = -dist; z <= dist; ++z)
-                {
-                    if(local_y + y + Chunk::SIZE * c.y < 1 || local_y + y + Chunk::SIZE * c.y >= World::HEIGHT*Chunk::SIZE || round(sqrt(x*x + y*y + z*z)) != dist)
-                        continue;
+    // Destroy everything in a sphere with a 3 block radius
+    const int dist = 3;
 
-                    //Explode other TNT blocks
-                    if(getBLOCK(c.getGlobalBlockRelative(local_x + x, local_y + y, local_z + z)) == BLOCK_TNT)
-                        explode(local_x + x, local_y + y, local_z + z, c);
-                    else if(getBLOCK(c.getGlobalBlockRelative(local_x + x, local_y + y, local_z + z)) != BLOCK_BEDROCK)
-                        c.changeGlobalBlockRelative(local_x + x, local_y + y, local_z + z, BLOCK_AIR);
-                }
-    }
+    for(int x = -dist; x <= dist; ++x)
+        for(int y = -dist; y <= dist; ++y)
+            for(int z = -dist; z <= dist; ++z)
+            {
+                if(local_y + y + Chunk::SIZE * c.y < 1 || local_y + y + Chunk::SIZE * c.y >= World::HEIGHT*Chunk::SIZE)
+                    continue;
+
+                if(x*x + y*y + z*z > dist*dist)
+                    continue;
+
+                //Explode other TNT blocks
+                auto block = getBLOCK(c.getGlobalBlockRelative(local_x + x, local_y + y, local_z + z));
+                if(block == BLOCK_TNT)
+                    explode(local_x + x, local_y + y, local_z + z, c);
+                else if(block != BLOCK_BEDROCK && block != BLOCK_AIR)
+                    c.changeGlobalBlockRelative(local_x + x, local_y + y, local_z + z, BLOCK_AIR);
+            }
 }
