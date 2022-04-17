481a992a7c5917a2d0c6026f9419d24e8d090a7c
m-doescode <80221594+m-doescode@users.noreply.github.com>
2022-04-17 11:58:58 +0200

    Reformatted document

--- blocklisttask.cpp
+++ blocklisttask.cpp
@@ -81,8 +81,7 @@ static const BLOCK_WDATA user_selectable[] = {
     BLOCK_PUMPKIN,
     BLOCK_PUMPKIN,
     BLOCK_PUMPKIN,
-    BLOCK_PUMPKIN
-};
+    BLOCK_PUMPKIN};
 
 //The values have to stay somewhere
 unsigned int BlockListTask::blocklist_top;
@@ -91,7 +90,7 @@ TEXTURE *BlockListTask::blocklist_background;
 
 unsigned int BlockListTask::screen_offset_y;
 
-constexpr int user_selectable_count = sizeof(user_selectable)/sizeof(*user_selectable);
+constexpr int user_selectable_count = sizeof(user_selectable) / sizeof(*user_selectable);
 
 BlockListTask::BlockListTask()
 {
@@ -100,7 +99,7 @@ BlockListTask::BlockListTask()
 
     static_assert(field_width * fields_x <= SCREEN_WIDTH, "fields_x too high");
     //static_assert(fields_x * fields_y >= sizeof(user_selectable)/sizeof(*user_selectable), "Not enough fields");
-    if(blocklist_height + current_inventory.height() > SCREEN_WIDTH)
+    if (blocklist_height + current_inventory.height() > SCREEN_WIDTH)
         printf("fields_y too high\n");
 
     blocklist_background = newTexture(blocklist_width, blocklist_height, 0, false);
@@ -113,7 +112,7 @@ BlockListTask::~BlockListTask()
 
 void BlockListTask::makeCurrent()
 {
-    if(!background_saved)
+    if (!background_saved)
         saveBackground();
 
     Task::makeCurrent();
@@ -128,26 +127,26 @@ void BlockListTask::render()
     int block_nr = screen_offset_y * fields_x;
     int screen_x, screen_y = blocklist_top + pad_y;
     // For each row
-    for(int y = screen_offset_y; y < fields_y + screen_offset_y; y++, screen_y += field_height)
+    for (int y = screen_offset_y; y < fields_y + screen_offset_y; y++, screen_y += field_height)
     {
         screen_x = blocklist_left + pad_x;
         // For each cell
-        for(int x = 0; x < fields_x; x++, screen_x += field_width)
+        for (int x = 0; x < fields_x; x++, screen_x += field_width)
         {
             //BLOCK_DOOR is twice as high, so center it manually
-            if(getBLOCK(user_selectable[block_nr]) == BLOCK_DOOR)
+            if (getBLOCK(user_selectable[block_nr]) == BLOCK_DOOR)
                 global_block_renderer.drawPreview(user_selectable[block_nr], *screen, screen_x + pad_x, screen_y + pad_y_door);
             else
                 global_block_renderer.drawPreview(user_selectable[block_nr], *screen, screen_x + pad_y, screen_y + pad_y);
-            
+
             // Increment cell count and exit loop once entire block list is rendered
             block_nr++;
-            if(block_nr == user_selectable_count)
+            if (block_nr == user_selectable_count)
                 goto end;
         }
     }
 
-    end:
+end:
 
     //Draw the selection indicator
     screen_x = blocklist_left + pad_x + field_width * (current_selection % fields_x);
@@ -160,53 +159,53 @@ void BlockListTask::render()
 
 void BlockListTask::logic()
 {
-    if(key_held_down)
-        key_held_down = keyPressed(KEY_NSPIRE_ESC) || keyPressed(KEY_NSPIRE_PERIOD) || keyPressed(KEY_NSPIRE_2) || keyPressed(KEY_NSPIRE_8) || keyPressed(KEY_NSPIRE_4) || keyPressed(KEY_NSPIRE_6) || keyPressed(KEY_NSPIRE_1) || keyPressed(KEY_NSPIRE_3) || keyPressed(KEY_NSPIRE_5) || keyPressed(KEY_NSPIRE_UP) || keyPressed(KEY_NSPIRE_DOWN) || keyPressed(KEY_NSPIRE_LEFT) || keyPressed(KEY_NSPIRE_RIGHT)  || keyPressed(KEY_NSPIRE_CLICK);
-    else if(keyPressed(KEY_NSPIRE_ESC) || keyPressed(KEY_NSPIRE_PERIOD))
+    if (key_held_down)
+        key_held_down = keyPressed(KEY_NSPIRE_ESC) || keyPressed(KEY_NSPIRE_PERIOD) || keyPressed(KEY_NSPIRE_2) || keyPressed(KEY_NSPIRE_8) || keyPressed(KEY_NSPIRE_4) || keyPressed(KEY_NSPIRE_6) || keyPressed(KEY_NSPIRE_1) || keyPressed(KEY_NSPIRE_3) || keyPressed(KEY_NSPIRE_5) || keyPressed(KEY_NSPIRE_UP) || keyPressed(KEY_NSPIRE_DOWN) || keyPressed(KEY_NSPIRE_LEFT) || keyPressed(KEY_NSPIRE_RIGHT) || keyPressed(KEY_NSPIRE_CLICK);
+    else if (keyPressed(KEY_NSPIRE_ESC) || keyPressed(KEY_NSPIRE_PERIOD))
     {
         world_task.makeCurrent();
 
         key_held_down = true;
     }
-    else if(keyPressed(KEY_NSPIRE_2) || keyPressed(KEY_NSPIRE_DOWN))
+    else if (keyPressed(KEY_NSPIRE_2) || keyPressed(KEY_NSPIRE_DOWN))
     {
         // Increment current cell by row size and overflow
         current_selection += fields_x;
-        if(current_selection >= user_selectable_count)
+        if (current_selection >= user_selectable_count)
             current_selection %= fields_x;
-        
+
         //if (current_selection >= fields_x * (fields_y - screen_offset_y))
-            screen_offset_y = std::max((current_selection / fields_x) - fields_y + 1, 0);
+        screen_offset_y = std::max((current_selection / fields_x) - fields_y + 1, 0);
 
         key_held_down = true;
     }
-    else if(keyPressed(KEY_NSPIRE_8) || keyPressed(KEY_NSPIRE_UP))
+    else if (keyPressed(KEY_NSPIRE_8) || keyPressed(KEY_NSPIRE_UP))
     {
         // (Here, there is a fast path, and a slow path for if the inventory size is not full)
         // Decrement current cell  by row size and underflow
-        if(current_selection >= fields_x)
+        if (current_selection >= fields_x)
             current_selection -= fields_x;
         else
         {
             // Floor off extra cells in last row and add current x (??)
             current_selection = ((user_selectable_count - 1) / fields_x) * fields_x + (current_selection % fields_x);
-            if(current_selection >= user_selectable_count)
+            if (current_selection >= user_selectable_count)
                 current_selection -= fields_x;
         }
 
         //if (current_selection >= fields_x * (fields_y - screen_offset_y))
-            screen_offset_y = std::max((current_selection / fields_x) - fields_y + 1, 0);
+        screen_offset_y = std::max((current_selection / fields_x) - fields_y + 1, 0);
 
         key_held_down = true;
     }
-    else if(keyPressed(KEY_NSPIRE_4) || keyPressed(KEY_NSPIRE_LEFT))
+    else if (keyPressed(KEY_NSPIRE_4) || keyPressed(KEY_NSPIRE_LEFT))
     {
         // If cell x is at 0, then send cursor to other side
-        if(current_selection % fields_x == 0)
+        if (current_selection % fields_x == 0)
         {
             current_selection += fields_x - 1;
             // And if there is less than the usual row size in the last row, then move back the cursor
-            if(current_selection >= user_selectable_count)
+            if (current_selection >= user_selectable_count)
                 current_selection = user_selectable_count - 1;
         }
         // Otherwise, just decrement the selection
@@ -215,10 +214,10 @@ void BlockListTask::logic()
 
         key_held_down = true;
     }
-    else if(keyPressed(KEY_NSPIRE_6) || keyPressed(KEY_NSPIRE_RIGHT))
+    else if (keyPressed(KEY_NSPIRE_6) || keyPressed(KEY_NSPIRE_RIGHT))
     {
         // If cell x is at the end of the row, move the cell back to the beginning of the row
-        if(current_selection % fields_x == fields_x - 1 || current_selection >= user_selectable_count - 1)
+        if (current_selection % fields_x == fields_x - 1 || current_selection >= user_selectable_count - 1)
             current_selection -= current_selection % fields_x;
         // Otherwise, increment the cell
         else
@@ -226,19 +225,19 @@ void BlockListTask::logic()
 
         key_held_down = true;
     }
-    else if(keyPressed(KEY_NSPIRE_1)) //Switch inventory slot
+    else if (keyPressed(KEY_NSPIRE_1)) //Switch inventory slot
     {
         current_inventory.previousSlot();
 
         key_held_down = true;
     }
-    else if(keyPressed(KEY_NSPIRE_3))
+    else if (keyPressed(KEY_NSPIRE_3))
     {
         current_inventory.nextSlot();
 
         key_held_down = true;
     }
-    else if(keyPressed(KEY_NSPIRE_5) || keyPressed(KEY_NSPIRE_CLICK))
+    else if (keyPressed(KEY_NSPIRE_5) || keyPressed(KEY_NSPIRE_CLICK))
     {
         current_inventory.currentSlot() = user_selectable[current_selection];
 
