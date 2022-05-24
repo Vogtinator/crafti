7fd0034dbe8e945a97eb49fb1d2a26878145ffc0
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-05-24 21:00:01 +0100

    Test for special destruction particles

--- blockrenderer.h
+++ blockrenderer.h
@@ -33,6 +33,9 @@ public:
     static void renderBillboard(int local_x, int local_y, int local_z, const TextureAtlasEntry &tex, Chunk &c);
     static void drawTextureAtlasEntry(TEXTURE &src, const TextureAtlasEntry &tex, TEXTURE &dest, const int dest_x, const int dest_y);
 
+    // Used for particles spawned on destruction
+    virtual const TerrainAtlasEntry &materialTexture(const BLOCK_WDATA block);
+
     virtual bool action(const BLOCK_WDATA block, const int local_x, const int local_y, const int local_z, Chunk &c) = 0; //Invoked by e.g. a right click
     virtual void tick(const BLOCK_WDATA block, int local_x, int local_y, int local_z, Chunk &c) = 0; //Invoked every now-and-then
     virtual void addedBlock(const BLOCK_WDATA block, int local_x, int local_y, int local_z, Chunk &c) = 0; //Invoked if this block has been placed
@@ -62,8 +65,6 @@ public:
     virtual AABB getAABB(const BLOCK_WDATA block, GLFix x, GLFix y, GLFix z) override;
 
     virtual void drawPreview(const BLOCK_WDATA block, TEXTURE &dest, const int x, const int y) override;
-    // Used for particles spawned on destruction
-    const TerrainAtlasEntry &materialTexture(const BLOCK_WDATA block);
 
     virtual bool action(const BLOCK_WDATA block, const int local_x, const int local_y, const int local_z, Chunk &c) override;
     virtual void tick(const BLOCK_WDATA block, int local_x, int local_y, int local_z, Chunk &c) override;
