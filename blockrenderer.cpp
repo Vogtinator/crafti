2d7f42b4a60133bc05ea284a1518bfdcf4a16c6c
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-05-25 13:26:46 +0100

    Fix the bug?

--- blockrenderer.cpp
+++ blockrenderer.cpp
@@ -386,6 +386,26 @@ const TerrainAtlasEntry &BlockRenderer::materialTexture(const BLOCK_WDATA block)
     return block_textures[BLOCK_STONE][BLOCK_FRONT];
 }
 
+const TerrainAtlasEntry &DumbBlockRenderer::materialTexture(const BLOCK_WDATA block) {
+    auto block_nr = getBLOCK(block);
+    if(block_nr <= BLOCK_NORMAL_LAST)
+        return block_textures[block_nr][BLOCK_FRONT];
+    else if(block_nr >= BLOCK_SPECIAL_START && block_nr <= BLOCK_SPECIAL_LAST)
+        return special_block_textures[block_nr - BLOCK_SPECIAL_START];
+
+    return block_textures[BLOCK_STONE][BLOCK_FRONT];
+}
+
+const TerrainAtlasEntry &NormalBlockRenderer::materialTexture(const BLOCK_WDATA block) {
+    auto block_nr = getBLOCK(block);
+    if(block_nr <= BLOCK_NORMAL_LAST)
+        return block_textures[block_nr][BLOCK_FRONT];
+    else if(block_nr >= BLOCK_SPECIAL_START && block_nr <= BLOCK_SPECIAL_LAST)
+        return special_block_textures[block_nr - BLOCK_SPECIAL_START];
+
+    return block_textures[BLOCK_STONE][BLOCK_FRONT];
+}
+
 const TerrainAtlasEntry &UniversalBlockRenderer::materialTexture(const BLOCK_WDATA block)
 {
     return map[getBLOCK(block)]->materialTexture(block);
