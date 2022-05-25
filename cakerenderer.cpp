d35ede58ab41c09d726233f6b65e58668800066e
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-05-25 20:16:28 +0100

    Fix edible cake AABB collision box

--- cakerenderer.cpp
+++ cakerenderer.cpp
@@ -117,19 +117,16 @@ AABB CakeRenderer::getAABB(const BLOCK_WDATA block, GLFix x, GLFix y, GLFix z)
             return {0, 0, 0, 0, 0, 0};
             break;
         case BLOCK_BACK:
-            //return {x + cake_offset, y, z + cake_offset, x + cake_offset, y + cake_height, z + cake_offset + cake_width};
-            return {0, 0, 0, 0, 0, 0};
+            return {x + cake_offset, y, z + cake_offset, x + cake_offset, y + cake_height, z + cake_offset + cake_size};
             break;
         case BLOCK_FRONT:
-            return {x + cake_offset, y, z + cake_offset, x + cake_offset + cake_width, y + cake_height, z + cake_offset + cake_size};
+            return {x + cake_offset, y, z + cake_offset + cake_size, x + cake_offset + cake_width, y + cake_height, z + cake_offset + cake_width};
             break;
         case BLOCK_LEFT:
-            //return {x + cake_offset + cake_size, y, z + cake_offset, x + cake_offset + cake_width, y + cake_height, z + cake_offset + cake_width};
-            return {0, 0, 0, 0, 0, 0};
+            return {x + cake_offset + cake_size, y, z + cake_offset, x + cake_offset + cake_width, y + cake_height, z + cake_offset + cake_width};
             break;
         case BLOCK_RIGHT: // LEFT X SIDE IS FACING YOU
-            //return {x + cake_offset, y, z + cake_offset, x + cake_offset + cake_size, y + cake_height, z + cake_offset + cake_width};
-            return {0, 0, 0, 0, 0, 0};
+            return {x + cake_offset, y, z + cake_offset, x + cake_offset + cake_size, y + cake_height, z + cake_offset + cake_width};
             break;
     }
     
