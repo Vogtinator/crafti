5fd76c0b58b38493946e07f2e5a7fb5f80cb6ac6
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-05-25 13:37:11 +0100

    Ditto?

--- blockrenderer.cpp
+++ blockrenderer.cpp
@@ -376,7 +376,7 @@ void UniversalBlockRenderer::drawPreview(const BLOCK_WDATA block, TEXTURE &dest,
     return map[getBLOCK(block)]->drawPreview(block, dest, x, y);
 }
 
-const TerrainAtlasEntry &DumbBlockRenderer::materialTexture(const BLOCK_WDATA block) {
+const TerrainAtlasEntry &BlockRenderer::materialTexture(const BLOCK_WDATA block) {
     auto block_nr = getBLOCK(block);
     if(block_nr <= BLOCK_NORMAL_LAST)
         return block_textures[block_nr][BLOCK_FRONT];
